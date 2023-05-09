ac29056663c062854fd09ec0d5918f36cd8530ca
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-05-10 00:04:13 +0530

    add

---
+++ Arrays/binarySearch.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class binarySearch {
+    public static int searchBinary(int num[], int target){
+        int start = 0;
+        int end = num.length-1;
+        
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(num[mid]==target){
+                return mid;
+            }
+            if(num[mid]>target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int num [] = {-1,0,3,5,9,12};
+        int target = 5;
+        System.out.println(searchBinary(num, target));
+    }
+}
