d53ecb8ad04eb68e10f8457cba971a35b155fffe
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-06-17 01:03:58 +0530

    ..

--- Arrays/maxSumSubarray.java
+++ Arrays/maxSumSubarray.java
@@ -1,24 +1,46 @@
-//Brute Force Approach
+// //Brute Force Approach
+
+// import java.util.*;
+// public class maxSumSubarray {
+//     public static void printMaxSum(int num[]){
+//         int maxSum = Integer.MIN_VALUE;
+//         for(int i =0; i<num.length; i++){
+//             for(int j = i; j<num.length; j++){
+//                 int currSum = 0;
+//                 for(int k = i; k<=j; k++){
+//                     currSum += num[k];
+//                 }
+//                 //System.out.println(currSum);
+//                 if(currSum>maxSum){
+//                     maxSum = currSum;
+//                 }
+//             }
+//         }
+//         System.out.println("MAx Sum = " + maxSum);
+//     }
+//     public static void main(String[] args) {
+//         int num[] = {1, -2, 6, -1, 3};
+//         printMaxSum(num);
+//     }
+// }
+
+//Kadane's Algorithm
+
 import java.util.*;
 public class maxSumSubarray {
-    public static void printMaxSum(int num[]){
+    public static void kadaneAlgo(int num[]){
         int maxSum = Integer.MIN_VALUE;
-        for(int i =0; i<num.length; i++){
-            for(int j = i; j<num.length; j++){
-                int currSum = 0;
-                for(int k = i; k<=j; k++){
-                    currSum += num[k];
-                }
-                //System.out.println(currSum);
-                if(currSum>maxSum){
-                    maxSum = currSum;
-                }
-            }
+        int currSum = 0;
+        for(int i = 0; i<num.length; i++){
+            currSum = Math.max(num[i], currSum + num[i]);
+            
+            maxSum = Math.max(currSum, maxSum);
         }
-        System.out.println("MAx Sum = " + maxSum);
+        System.out.println("Max sum is: " + maxSum);
     }
     public static void main(String[] args) {
         int num[] = {1, -2, 6, -1, 3};
-        printMaxSum(num);
+        
+        kadaneAlgo(num);
     }
 }
