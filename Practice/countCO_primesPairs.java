22622a9c6421f38bcf66cfe8369c3f725ea9625c
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-08-14 20:44:32 +0530

    ..

---
+++ Practice/countCO_primesPairs.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class countCO_primesPairs {
+
+    private static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int countPairs(int arr[]) {
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (findGCD(arr[i], arr[j]) == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(countPairs(arr));
+    }
+}
