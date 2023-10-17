eaf7a93a4bb3851d44034e3a4e3213d75401a333
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-10-17 12:32:35 +0530

    ..

---
+++ Practice/main.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] count = new int[26];
+        boolean[] visited = new boolean[26];
+        StringBuilder result = new StringBuilder();
+        
+        // Count the frequency of each character in the input string
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        
+        // Process each character in ascending order
+        for (char c : s.toCharArray()) {
+            count[c - 'a']--; // Decrement the frequency of the current character
+            
+            // Skip if the character has already been visited
+            if (visited[c - 'a']) {
+                continue;
+            }
+            
+            // Remove smaller characters from the result if they occur later
+            while (result.length() > 0 && result.charAt(result.length() - 1) < c && count[result.charAt(result.length() - 1) - 'a'] > 0) {
+                visited[result.charAt(result.length() - 1) - 'a'] = false;
+                result.deleteCharAt(result.length() - 1);
+            }
+            
+            // Append the current character to the result and mark it as visited
+            result.append(c);
+            visited[c - 'a'] = true;
+        }
+        
+        return result.toString();
+    }
+}
